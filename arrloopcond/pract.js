7e15bbff4c691466cfa7fa96d3ff03979d6ac805
SilentChaos <avanishm471@gmail.com>
2023-02-16 18:42:54 +0530

    nothing

---
+++ arrloopcond/pract.js
@@ -0,0 +1,17 @@
+
+
+let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+let appleShelf = document.getElementById("apple-shelf")
+let orangeShelf = document.getElementById("orange-shelf")
+
+
+function  shelf() {
+    for (let i = 0; i < fruit.length; i++) {
+        if (fruit[i] === "🍎") {
+            appleShelf.textContent += "🍎"
+        } else if (fruit[i] === "🍊") {
+            orangeShelf.textContent += "🍊"
+        }
+    }
+}
+shelf()
